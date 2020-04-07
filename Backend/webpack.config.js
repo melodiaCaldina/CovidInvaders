f34e0a543827be3bdc12ecad892a49454aa53246
melo <melodia.nocturnis@gmail.com>
2020-04-07 10:47:32 +0200

    update assets pack

--- Backend/webpack.config.js
+++ Backend/webpack.config.js
@@ -26,7 +26,18 @@ Encore
     .addEntry('app', './assets/js/app.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
+    .copyFiles({
+         from: './assets',
 
+         // optional target path, relative to the output dir
+         //to: 'images/[path][name].[ext]',
+
+         // if versioning is enabled, add the file hash too
+         //to: 'images/[path][name].[hash:8].[ext]',
+
+         // only copy files matching this pattern
+         //pattern: /\.(png|jpg|jpeg)$/
+     })
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
 
